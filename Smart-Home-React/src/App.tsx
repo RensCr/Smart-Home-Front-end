05e667fbe2a3b512ac5cfd85be61dd7272b151d6
RensCr <rens.crijns@Outlook.com>
2024-10-18 18:00:10 +0200

    Aanmaak login account + jwttoken uitlezen

--- Smart-Home-React/src/App.tsx
+++ Smart-Home-React/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages";
 import Toevoegen from "./pages/Toevoegen";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Aanmaak from "./pages/Accountaanmaak";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="" element={<Index />} />
         <Route path="/toevoegen" element={<Toevoegen />} />
+        <Route path="/Aanmelden" element={<Aanmaak />} />
+        <Route path="/Inloggen" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- Smart-Home-React/src/pages/index.tsx
+++ Smart-Home-React/src/pages/index.tsx
@@ -2,8 +2,35 @@ import "../App.css";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import "../Css/Home.css";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Index() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkJwtToken = () => {
+      const cookies = document.cookie.split("; ");
+      const jwtTokenCookie = cookies.find((cookie) =>
+        cookie.startsWith("jwtToken=")
+      );
+      if (jwtTokenCookie) {
+        console.log("jwtToken cookie found");
+        const isSecure = jwtTokenCookie.includes("Secure");
+        if (isSecure) {
+          console.log("jwtToken cookie is secure");
+        } else {
+          console.log("jwtToken cookie is not secure");
+        }
+      } else {
+        console.log("jwtToken cookie not found, redirecting to login");
+        navigate("/inloggen");
+      }
+    };
+
+    checkJwtToken();
+  }, [navigate]);
+
   return (
     <>
       <Header></Header>
@@ -20,81 +47,6 @@ function Index() {
                 </a>
                 <a href="#" className="btn btn-uit">
                   Uit
-                </a>
-                <a href="#" className="btn btn-slim">
-                  Slim
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-3">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Slim lichtje - Slaapkamer jantje</h5>
-                <p className="card-text">Toestand : Aan</p>
-                <br></br>
-                <a href="#" className="btn btn-aan">
-                  Aan
-                </a>
-                <a href="#" className="btn btn-uit">
-                  Uit
-                </a>
-                <a href="#" className="btn btn-slim">
-                  Slim
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-3">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Slim licht - Slaapkamer jantje</h5>
-                <p className="card-text">Toestand : Aan</p>
-                <br></br>
-                <a href="#" className="btn btn-aan">
-                  Aan
-                </a>
-                <a href="#" className="btn btn-uit">
-                  Uit
-                </a>
-                <a href="#" className="btn btn-slim">
-                  Slim
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-3">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Slim licht - Slaapkamer jantje</h5>
-                <p className="card-text">Toestand : Aan</p>
-                <br></br>
-                <a href="#" className="btn btn-aan">
-                  Aan
-                </a>
-                <a href="#" className="btn btn-uit">
-                  Uit
-                </a>
-                <a href="#" className="btn btn-slim">
-                  Slim
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-3">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Slim licht - Slaapkamer jantje</h5>
-                <p className="card-text">Toestand : Aan</p>
-                <br></br>
-                <a href="#" className="btn btn-aan">
-                  Aan
-                </a>
-                <a href="#" className="btn btn-uit">
-                  Uit
-                </a>
-                <a href="#" className="btn btn-slim">
-                  Slim
                 </a>
               </div>
             </div>
